eb238adfb224257b8060f35385be9b87f0362760
yoelhaim <youssefelhaimer8@gmail.com>
2022-11-21 20:29:47 +0100

    reemove  object files :rocket:

--- src/check_errors_map.c
+++ src/check_errors_map.c
@@ -6,56 +6,18 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 19:15:13 by pro               #+#    #+#             */
-/*   Updated: 2022/11/20 14:36:30 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/11/21 14:39:42 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int check_errors(char **value)
+int	check_colors_floor(char *color, t_cub3d *cubmap)
 {
-	int fd;
-	if (!value)
-		return (printf("error\n"), 0);
-	if (ft_strlen(value[0]) != 2)
-		return (printf("error name \n"), 0);
-	fd = open(ft_strtrim((strstr(value[0], "./") + 2), "\n"), O_RDONLY);
-	if (fd < 0)
-		return (printf("error open texture  %s\n", value[0]), 0);
-
-	return (1);
-}
-
-int check_exten(char *filename)
-{
-	int fd;
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-	return (1);
-}
-
-int ft_is_digit(char *str)
-{
-	int i;
-
-	i = -1;
-	while (str[++i])
-	{
-		if ((str[i] < '0' || str[i] > '9'))
-			return (1);
-	}
-	return (0);
-}
-
-int check_colors_floor(char *color, t_cub3d *cubmap)
-{
-	char **splited;
-	int i;
+	char	**splited;
+	int		i;
 
 	i = 0;
-
 	splited = ft_split(color, ',');
 	while (splited[i])
 	{
@@ -63,7 +25,6 @@ int check_colors_floor(char *color, t_cub3d *cubmap)
 			return (0);
 		i++;
 	}
-
 	if (i != 3)
 		return (0);
 	i = -1;
@@ -78,13 +39,12 @@ int check_colors_floor(char *color, t_cub3d *cubmap)
 	return (1);
 }
 
-int check_colors_ciel(char *color, t_cub3d *cubmap)
+int	check_colors_ciel(char *color, t_cub3d *cubmap)
 {
-	char **splited;
-	int i;
+	char	**splited;
+	int		i;
 
 	i = 0;
-
 	splited = ft_split(color, ',');
 	while (splited[i])
 	{
@@ -106,9 +66,9 @@ int check_colors_ciel(char *color, t_cub3d *cubmap)
 	return (1);
 }
 
-int check_floor_ceil(int *str)
+int	check_floor_ceil(int *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < 3)
@@ -120,18 +80,20 @@ int check_floor_ceil(int *str)
 	return (1);
 }
 
-int is_open(char *namefile)
+int	is_open(char *namefile)
 {
-	int fd;
+	int	fd;
 
 	fd = open(namefile, O_RDONLY);
 	close(fd);
 	return (fd);
 }
 
-int check_file_texture(t_cub3d *texture)
+int	check_file_texture(t_cub3d *texture)
 {
-	if (is_open(texture->no) == -1 || is_open(texture->so) == -1 || is_open(texture->ea) == -1 || is_open(texture->we) == -1)
+	if (is_open(texture->no) == -1 || \
+	is_open(texture->so) == -1 || \
+	is_open(texture->ea) == -1 || is_open(texture->we) == -1)
 		return (0);
 	return (1);
 }

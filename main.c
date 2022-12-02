72fd7eaedb3147833d602ee884b2186bf5aadc5b
yoelhaim <youssefelhaimer8@gmail.com>
2022-12-02 13:04:27 +0100

    add  new func get and check map

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int ft_parsing(int ac, char *file_name, t_cub3d *cubmap)
 		return (ft_putstr_fd("Argument not valid\n", 2), 1);
 	if (!check_exten(file_name) || (!check_name_exc(file_name)))
 		return (ft_putstr_fd("error :  open  map !\n", 2), 1);
-	if (!read_to_file(file_name, cubmap) || !check_maps(cubmap, file_name))
+	if (!read_to_file(file_name, cubmap))
 		return (ft_putstr_fd("error check map\n", 2), 1);
 	if (!check_floor_ceil(cubmap->floor) || !check_floor_ceil(cubmap->ciel))
 		return (ft_putstr_fd("error color\n", 2), 1);
@@ -60,6 +60,7 @@ int	main(int ac, char **av)
 
     if (ft_parsing(ac, av[1], &cubmap))
         return 1;
+	// return 1;
 	ft_init(&data, &cubmap);
 	ft_main(&data);
 	ft_end(data);
